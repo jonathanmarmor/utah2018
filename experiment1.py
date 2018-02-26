473e4c3b7af83ea74ded757ed0d4218023a4bec5
Jonathan Marmor <jmarmor@gmail.com>
2018-02-25 21:58:16 -0500

    Some work in progress from long ago

--- experiment1.py
+++ experiment1.py
@@ -127,33 +127,34 @@ class Experiment1(object):
                 # 'clarinet',
                 # 'alto_saxophone',
                 # 'trumpet',
-                # 'bass'
+                'bass'
             ),
             starting_tempo_bpm=160
         )
         beat_duration = 60.0 / self.music.starting_tempo_bpm
 
+        self.bass = m.bass
 
 
         self.clusters = [m.ob, m.ob2, m.ob3]
         cluster_lowest_pitch = 70
         for i in self.clusters:
             i.cluster_range = [p for p in i.range if p >= cluster_lowest_pitch]
-        self.clusters[0].add_note(pitch=84, duration=2)
-        self.clusters[1].add_note(pitch=83, duration=2)
-        self.clusters[2].add_note(pitch=82, duration=2)
+        self.clusters[0].add_note(pitch=82, duration=2)
+        self.clusters[1].add_note(pitch=81, duration=2)
+        self.clusters[2].add_note(pitch=80, duration=2)
         while beat_duration * self.clusters[0].duration() < 120.0:
-            self.next()
+            self.next(bass=True)
 
         self.clusters = [m.a_rec, m.a_rec2, m.a_rec3]
         cluster_lowest_pitch = 70
         for i in self.clusters:
             i.cluster_range = [p for p in i.range if p >= cluster_lowest_pitch]
-        self.clusters[0].add_note(pitch=84, duration=2)
-        self.clusters[1].add_note(pitch=83, duration=2)
-        self.clusters[2].add_note(pitch=82, duration=2)
+        self.clusters[0].add_note(pitch=85, duration=2)
+        self.clusters[1].add_note(pitch=84, duration=2)
+        self.clusters[2].add_note(pitch=83, duration=2)
         while beat_duration * self.clusters[0].duration() < 120.0:
-            self.next()
+            self.next(bass=False)
 
 
 
@@ -231,11 +232,12 @@ class Experiment1(object):
         while self.music.duration_seconds() < duration:
             self.next()
 
-    def next(self):
+    def next(self, bass=False):
         self.clusters_next()
         # self.thirds_next()
         # self.violin_next()
-        # self.bass_next()
+        if bass:
+            self.bass_next()
 
     def clusters_next(self):
         changing, not_changing = self.clusters_pick_changing_instrument()
@@ -310,9 +312,6 @@ class Experiment1(object):
 
         not_changing = [w for w in self.clusters if w is not changing]
 
-        print
-        print 'not_changing', not_changing
-        print
         return changing, not_changing
 
     def clusters_pick_new_pitch(self, changing, not_changing, allow_repeated_pitch=False):
@@ -373,64 +372,70 @@ class Experiment1(object):
 
         return new_pitch
 
-    # def bass_next(self):
-    #     bar_number = self.bass.duration() // 4
-    #     bar_in_progression = bar_number % len(BLUES_PROGRESSION)
+    def bass_next(self):
+        bar_number = self.bass.duration() // 4
+        bar_in_progression = bar_number % len(BLUES_PROGRESSION)
 
-    #     beat_number = int(self.bass.duration() % 4)
-
-    #     previous_pitch = self.bass.get_last_pitched().pitch
-
-    #     pitch_options = []
-    #     weights = []
-
-    #     available_pitches = range(35, 52)
-    #     for pitch_option in available_pitches:
-    #         weight = 1.0
-
-    #         if pitch_option % 12 not in SCALE:
-    #             weight = .1
-
-    #         if pitch_option % 12 in BLUES_PROGRESSION[bar_in_progression]:
-    #             weight = 8.0
+        beat_number = int(self.bass.duration() % 4)
 
 
-    #         # The further away the new pitch from the previous pitch, the lower the weight
-    #         distance = abs(previous_pitch - pitch_option)
+        last_pitched_note = self.bass.get_last_pitched()
+        if last_pitched_note:
+            previous_pitch = self.bass.get_last_pitched().pitch
+        else:
+            previous_pitch = 48
 
-    #         if distance == 0:
-    #             weight = 1.0
-    #         else:
-    #             if distance < 3:
-    #                 weight = weight * 8
-    #             elif distance < 6:
-    #                 weight = weight * 4
-    #             # elif distance < 9:
-    #             #     weight = weight * 2
-    #             elif distance > 12:
-    #                 weight = .125
 
-    #         pitch_options.append(pitch_option)
-    #         weights.append(weight)
+        pitch_options = []
+        weights = []
 
-    #     pitch = weighted_choice(pitch_options, weights)
+        available_pitches = range(35, 52)
+        for pitch_option in available_pitches:
+            weight = 1.0
 
-    #     if beat_number == 0:
-    #         duration_options = [1,  2,  3, 4, 5, 6, 7, 8]
-    #         duration_weights = [35, 24, 2, 6, 1, 1, 1, 2]
-    #     elif beat_number == 1:
-    #         duration_options = [1,  2,  3,  4, 5, 6, 7]
-    #         duration_weights = [35, 12, 16, 1, 1, 1, 3]
-    #     elif beat_number == 2:
-    #         duration_options = [1,  2,  4, 6]
-    #         duration_weights = [24, 24, 1, 2]
-    #     elif beat_number == 3:
-    #         duration_options = [1,  2, 5]
-    #         duration_weights = [40, 2, 5]
+            if pitch_option % 12 not in SCALE:
+                weight = .1
 
-    #     duration = weighted_choice(duration_options, duration_weights)
+            if pitch_option % 12 in BLUES_PROGRESSION[bar_in_progression]:
+                weight = 8.0
 
-    #     self.bass.add_note(pitch=pitch, duration=duration)
+
+            # The further away the new pitch from the previous pitch, the lower the weight
+            distance = abs(previous_pitch - pitch_option)
+
+            if distance == 0:
+                weight = 1.0
+            else:
+                if distance < 3:
+                    weight = weight * 8
+                elif distance < 6:
+                    weight = weight * 4
+                # elif distance < 9:
+                #     weight = weight * 2
+                elif distance > 12:
+                    weight = .125
+
+            pitch_options.append(pitch_option)
+            weights.append(weight)
+
+        pitch = weighted_choice(pitch_options, weights)
+
+        if beat_number == 0:
+            duration_options = [1,  2,  3, 4, 5, 6, 7, 8]
+            duration_weights = [35, 24, 2, 6, 1, 1, 1, 2]
+        elif beat_number == 1:
+            duration_options = [1,  2,  3,  4, 5, 6, 7]
+            duration_weights = [35, 12, 16, 1, 1, 1, 3]
+        elif beat_number == 2:
+            duration_options = [1,  2,  4, 6]
+            duration_weights = [24, 24, 1, 2]
+        elif beat_number == 3:
+            duration_options = [1,  2, 5]
+            duration_weights = [40, 2, 5]
+
+        duration = weighted_choice(duration_options, duration_weights)
+
+        self.bass.add_note(pitch=pitch, duration=duration)
 
 
 if __name__ == '__main__':
